07a83c2b220f4500e2388f9045467fa23ce4bf57
NuwanS rajapaksha <155893660+NuwanSrajapasha@users.noreply.github.com>
2024-11-25 10:07:26 +0530

    create JFrame

---
+++ Frame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+class Example{
+    public static void main(String[] args) {
+        JFrame f1=new JFrame();
+        f1.setSize(300,300);
+        f1.setTitle("My First Frame");
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setVisible(true);
+        
+    }
+}
