b80709886a410feb2f3e03df50665a0e2ffa4841
NuwanS rajapaksha <155893660+NuwanSrajapasha@users.noreply.github.com>
2024-11-27 10:41:12 +0530

    Culculator Ui with array

--- BorderLayoutWindow.java
+++ BorderLayoutWindow.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 
 class GRid extends JFrame{
-    JButton btn7,btn8,btn9,btnstr,btn4,btn5,btn6,btndiv,btn1,btn2,btn3,btnadd,btn0,btnf,btneq,btnmi;
+    JButton [] btnArr;
 
     GRid(){
         setSize(500,300);
@@ -14,71 +14,19 @@ class GRid extends JFrame{
 
         Font font=new Font("",1,25);
 
-        btn7=new JButton("7");
-        btn7.setFont(font);
-        add(btn7);
 
-        btn8=new JButton("8");
-        btn8.setFont(font);
-        add(btn8);
+        String text[]={"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
+        btnArr=new JButton[16];
 
-        btn9=new JButton("9");
-        btn9.setFont(font);
-        add(btn9);
+        for(int i=0;i<16;i++){
+            btnArr[i]=new JButton(text[i]);
+            btnArr[i].setFont(font);
+            add(btnArr[i]);
 
-        btnstr=new JButton("*");
-        btnstr.setFont(font);
-        add(btnstr);
+            setVisible(true);
+        }
 
-        btn4=new JButton("4");
-        btn4.setFont(font);
-        add(btn4);
-
-        btn5=new JButton("5");
-        btn5.setFont(font);
-        add(btn5);
-
-        btn6=new JButton("6");
-        btn6.setFont(font);
-        add(btn6);
-
-        btndiv=new JButton("/");
-        btndiv.setFont(font);
-        add(btndiv);
-
-        btn1=new JButton("1");
-        btn1.setFont(font);
-        add(btn1);
-
-        btn2=new JButton("2");
-        btn2.setFont(font);
-        add(btn2);
-
-        btn3=new JButton("3");
-        btn3.setFont(font);
-        add(btn3);
-
-        btnadd=new JButton("+");
-        btnadd.setFont(font);
-        add(btnadd);
-
-        btn0=new JButton("0");
-        btn0.setFont(font);
-        add(btn0);
-
-        btnf=new JButton(".");
-        btnf.setFont(font);
-        add(btnf);
-
-        btneq=new JButton("=");
-        btneq.setFont(font);
-        add(btneq);
-
-        btnmi=new JButton("-");
-        btnmi.setFont(font);
-        add(btnmi);
-
-        setVisible(true);
+        
     }
 }
 class Example{
